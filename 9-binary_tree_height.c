1e51f60d073881f0ca5a8ee814e118b98b81086b
Adetule-ayomide <adetuleayomide@gmail.com>
2023-11-01 05:42:16 -0700

    Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,12 +9,12 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t lhs = 0, rhs = 0;
+	size_t left_height = 0, right_height = 0;
 
 	if (tree)
 	{
-		lhs = (tree->left ? binary_tree_height(tree->left) + 1 : 0);
-		rhs = (tree->right ? binary_tree_height(tree->right) + 1 : 0);
+		left_height = (tree->left ? binary_tree_height(tree->left) + 1 : 0);
+		right_height = (tree->right ? binary_tree_height(tree->right) + 1 : 0);
 	}
-	return (lhs > rhs ? lhs : rhs);
+	return (left_height > right_height ? left_height : right_height);
 }
